18696f03a76b84a3a756f34525befab67d08c536
fatima-nigum <nigumfatimaa@gmail.com>
2026-01-28 18:16:29 +0500

    Resolve merge conflicts from Nigum branch

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -3,8 +3,7 @@ const { signup, login } = require("../controllers/authcontroller");
 
 const authRouter = express.Router();
 
-/* ================= AUTH ROUTES ================= */
+
 authRouter.post("/signup", signup);
 authRouter.post("/login", login);
-
 module.exports = { authRouter };
